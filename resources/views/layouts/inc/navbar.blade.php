18b1f320179bd044274c23a08a17644989ae125f
Novitasalsabila <salsblavta@gmail.com>
2026-09-25 14:35:36 +0700

    mengghapus profile admin

--- resources/views/layouts/inc/navbar.blade.php
+++ resources/views/layouts/inc/navbar.blade.php
@@ -22,12 +22,6 @@
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                 aria-labelledby="userDropdown">
 
-                <!-- Profile -->
-                <a class="dropdown-item" href="">
-                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Profile
-                </a>
-
                 <div class="dropdown-divider"></div>
 
                 <!-- Logout -->
